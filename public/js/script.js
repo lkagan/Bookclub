2c3a320d414874d7fde21babbf120fea0cbcd261
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-04-06 22:13:33 -0400

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("Bookclub JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Bookclub";
+
+module.exports = MONGO_URI;
