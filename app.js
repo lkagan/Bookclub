c54272d3511957de2951dc83c16578e10b81b157
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-04-08 21:07:46 -0400

    layout completed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ require('./config/session.config')(app);
 const capitalized = require("./utils/capitalized");
 const projectName = "Bookclub";
 
-app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalized(projectName)}`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index.routes");
