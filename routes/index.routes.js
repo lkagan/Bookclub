51bd1dcfe786a8971112d80871c72ed69d6b3200
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-04-16 16:46:37 -0400

    edit profile working

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,46 +1,76 @@
-const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedIn = require("../middleware/isLoggedIn");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 const cloudinary = require("../config/cloudinary.config");
 
 const User = require("../models/User.model");
 
-
-
 /* GET home page */
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 //Details page get route
-router.get('/details', (req, res, next) => {
-    res.render('page-details');
+router.get("/details", (req, res, next) => {
+  res.render("page-details");
 });
 
 //Contact page get route
-router.get('/contact', (req, res, next) => {
-    res.render('contact');
+router.get("/contact", (req, res, next) => {
+  res.render("contact");
 });
 
 // router.get("/profile", isLoggedIn, (req, res, next) => {
-router.get('/profile', (req, res, next) => {
-    res.render('profile');
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
 });
 
-router.post('/profile', cloudinary.single("profile-picture"), (req, res, next) => {
-    console.log(req.file.path);
-    User.findByIdAndUpdate(req.session.currentUser._id, {profilePicture: req.file.path}, {new: true})
-    .then(updatedUser => {
+// router.post('/profile', cloudinary.single("profile-picture"), (req, res, next) => {
+//     User.findByIdAndUpdate(req.session.currentUser._id, {profilePicture: req.file.path}, {new: true})
+//     .then(updatedUser => {
+//         req.session.currentUser = updatedUser;
+//         res.redirect('/profile');
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         next(err);
+//     });
+// });
+
+router.get("/user/edit-user", (req, res, next) => {
+  res.render("user/edit-user");
+});
+
+router.post("/user/edit-user", cloudinary.single("profile-picture"),(req, res, next) => {
+    // console.log(req.file)
+    let path;
+
+    if(req.file){
+        path = req.file.path;
+    }
+
+    // if(req.body.password){}
+    
+    User.findByIdAndUpdate(
+      req.session.currentUser._id,
+      {
+        username: req.body.username,
+        email: req.body.email,
+        // password: req.body.password,
+        profilePicture: path,
+      },
+      { new: true }
+    )
+      .then((updatedUser) => {
         req.session.currentUser = updatedUser;
-        res.redirect('/profile');
-    })
-    .catch(err => {
-        console.log(err)
+        res.redirect("/profile");
+      })
+      .catch((err) => {
+        console.log(err);
         next(err);
-    });
-});
-
-
+      });
+  }
+);
 
 module.exports = router;
